66c6dceeefd6053884cfc610aa5de069be84f850
Zac Spitzer <zac.spitzer@gmail.com>
2023-07-08 08:45:58 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -6,6 +6,7 @@ exitcode=0
 
 box stop name="presidetests"
 box start directory="./" serverConfigFile="./server-presidetests.json"
+sleep 15
 box testbox run verbose=false || exitcode=1
 box stop name="presidetests"
 
